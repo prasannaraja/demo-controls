ffa2f71f722ea85485b5505359f6cbf3c1f9b75e
Prasanna Raja <prasannaraja@msn.com>
2020-09-19 16:00:27 +0530

    home, pagenotfound and shared components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SharedModule } from "../app/shared/shared.module";
+import { APPErrorHandler } from "../app/core/error-handler";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: APPErrorHandler,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/control/primeng/lazy-grid/lazy-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy-grid',
+  templateUrl: './lazy-grid.component.html',
+  styleUrls: ['./lazy-grid.component.css']
+})
+export class LazyGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
